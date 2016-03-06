381eec6fe13792b764c9b712742659bd763e0228
曾偉珉 <ryan10328@hotmail.com>
2016-03-06 20:32:14 +0800

    加入檢視表並對應至Edmx

--- MvcHomework01/Models/CrmModel.Context.cs
+++ MvcHomework01/Models/CrmModel.Context.cs
@@ -28,5 +28,6 @@ namespace MvcHomework01.Models
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
+        public virtual DbSet<VWCustomerInformation> VWCustomerInformations { get; set; }
     }
 }

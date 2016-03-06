7bdaa2c118beb4c2e2209cd6a121ed0b6793dc06
曾偉珉 <ryan10328@hotmail.com>
2016-03-06 19:29:48 +0800

    First Commit

---
+++ MvcHomework01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcHomework01.Startup))]
+namespace MvcHomework01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

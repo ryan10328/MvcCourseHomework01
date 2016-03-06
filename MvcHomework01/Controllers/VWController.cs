41fc9bbf3cd87f18c11b3343a07c191a91de1ba0
曾偉珉 <ryan10328@hotmail.com>
2016-03-06 23:39:56 +0800

    [Add] 加入檢視頁面

---
+++ MvcHomework01/Controllers/VWController.cs
@@ -0,0 +1,20 @@
+﻿using MvcHomework01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcHomework01.Controllers
+{
+    public class VWController : Controller
+    {
+        private CrmEntities db = new CrmEntities();
+        // GET: VW
+        public ActionResult Index()
+        {
+            var result = db.VWCustomerInformations.ToList();
+            return View(result);
+        }
+    }
+}
---
+++ MvcHomework01/Models/VWCustomerInformation.Partial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcHomework01.Models
+{
+
+    [MetadataType(typeof(VWCustomerInformationMetaData))]
+    public partial class VWCustomerInformation
+    {
+    }
+
+    public partial class VWCustomerInformationMetaData
+    {
+        [Display(Name = "客戶名稱")]
+        public string CustomerName { get; set; }
+        [Display(Name = "銀行帳戶數量")]
+        public Nullable<int> BankCount { get; set; }
+        [Display(Name = "聯絡人數量")]
+        public Nullable<int> ContactCount { get; set; }
+    }
+}
